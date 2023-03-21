e6fa5abe513a455b0c0949be6566713911dd2e64
AlexBrown4020 <balex3497@gmail.com>
2023-03-21 13:12:10 +0900

    Feat: Implement basic home page

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -5,6 +5,13 @@ export default function Home () {
         <main>
             <h1>Welcome to Life in Balance:</h1>
             <h2>Hatha Yoga with Julie</h2>
+            <img alt='main home' src='1679371570821.jpg'/>
+            <p>
+                Everyone could use more yoga in their lives, so I hope to see you on the mat soon!
+            </p>
+            <p>
+                Happiness and contentment to you.
+            </p>
         </main>
     )
 }
