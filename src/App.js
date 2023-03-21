b113d890847b36d155b58d6c469f6c1cf9576e1a
AlexBrown4020 <balex3497@gmail.com>
2023-03-21 13:18:39 +0900

    Feat: Basic layout for navbar

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import Home from './pages/home/Home';
 import './App.css';
+import Navbar from './components/navbar/Navbar';
 
 function App() {
   return (
     <div className="App">
+      <Navbar/>
       <Home/>
     </div>
   );
---
+++ src/components/navbar/Navbar.js
@@ -0,0 +1,12 @@
+import './navbar.css';
+
+export default function Navbar () {
+    return (
+        <nav>
+            <button><a>Home</a></button>
+            <button><a>Classes</a></button>
+            <button><a>Contact</a></button>
+            <button><a>About</a></button>
+        </nav>
+    )
+}
