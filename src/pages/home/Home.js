9b38f2d7701954b6c1ae26eff8464f3d73cb1527
AlexBrown4020 <balex3497@gmail.com>
2023-03-21 12:16:31 +0900

    Feat: relocate file location, add title to home page

---
+++ src/pages/home/Home.js
@@ -0,0 +1,10 @@
+import './home.css';
+
+export default function Home () {
+    return (
+        <main>
+            <h1>Welcome to Life in Balance:</h1>
+            <h2>Hatha Yoga with Julie</h2>
+        </main>
+    )
+}
