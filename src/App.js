0e62b521785d13dc712038366576df382631e8d2
AlexBrown4020 <balex3497@gmail.com>
2023-03-21 13:56:48 +0900

    Feat: Implement contact page

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import Home from './pages/home/Home';
 import './App.css';
 import Navbar from './components/navbar/Navbar';
 import Classes from './pages/classes/Classes';
+import Contact from './pages/contact/Contact';
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Navbar/>
       <Home/>
       <Classes/>
+      <Contact/>
     </div>
   );
 }
---
+++ src/pages/contact/Contact.js
@@ -0,0 +1,20 @@
+import './contact.css';
+
+export default function Contact () {
+    return (
+        <div>
+            <link rel="stylesheet" href="https://cdn.jsdelivr.net/gh/devicons/devicon@v2.15.1/devicon.min.css"></link>
+            <h3>Contact Me</h3>
+            <p>Send an email or Facebook message to set up your sessions.
+                Be sure to check me out on Instagram!
+            </p>
+            <img id='user-contact' alt='user-contact' src='1679288947320.jpg'/>
+            <div id='social-media-container'>
+                <img alt='facebook' src='facebook.png'/>
+                <img alt='mail' src='mail.png'/>
+                <img alt='instagram' src='instagram.png'/>
+            </div>
+
+        </div>
+    )
+}
