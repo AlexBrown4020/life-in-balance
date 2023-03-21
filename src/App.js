8932e5f538f05c450caa953be807022af67b7656
AlexBrown4020 <balex3497@gmail.com>
2023-03-21 13:35:34 +0900

    Feat: Implement class and schedule page

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import Home from './pages/home/Home';
 import './App.css';
 import Navbar from './components/navbar/Navbar';
+import Classes from './pages/classes/Classes';
 
 function App() {
   return (
     <div className="App">
       <Navbar/>
       <Home/>
+      <Classes/>
     </div>
   );
 }
---
+++ src/pages/classes/Classes.js
@@ -0,0 +1,37 @@
+import './classes.css';
+
+export default function Classes () {
+    return (
+        <div>
+            <h3>Classes & Schedule</h3>
+            <section>
+                Friday Livestream:
+                <p>
+                    1 hour group session held online at 9AM, every Friday
+                </p>
+                <ul>
+                    <li>Monthly membership at £6 per class</li>
+                    <li>Or £7 pr class</li>
+                </ul>
+            </section>
+            <section>
+                Zoom Session:
+                <ul>
+                    <li>£25 per 60 minutes</li>
+                    <li>Camera and a stable internet connection</li>
+                </ul>
+            </section>
+            <section>
+                Home Visit:
+                <ul>
+                    <li>£35-£45 per 75 minutes</li>
+                    <li>Equipment available on request</li>
+                </ul>
+            </section>
+            <p>
+                *Block bookings and group sessions are available
+                **Home visits are limited by distance/location
+            </p>
+        </div>
+    )
+}
