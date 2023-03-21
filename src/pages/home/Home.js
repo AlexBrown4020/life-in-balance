390a88856ff11c6ed53de717bd761d5c7d15abcf
AlexBrown4020 <balex3497@gmail.com>
2023-03-21 15:11:53 +0900

    feat: Simplify hoem page

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -6,12 +6,6 @@ export default function Home () {
             <h1>Welcome to Life in Balance:</h1>
             <h2>Hatha Yoga with Julie</h2>
             <img alt='main home' src='1679372360937.jpg'/>
-            <p>
-                Everyone could use more yoga in their lives, so I hope to see you on the mat soon!
-            </p>
-            <p>
-                Happiness and contentment to you.
-            </p>
         </main>
     )
 }
