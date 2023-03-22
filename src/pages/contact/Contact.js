42cb40f73dc4acb2be6631d094698485c0245555
AlexBrown4020 <balex3497@gmail.com>
2023-03-22 17:10:42 +0900

    Feat: client image adjustment

--- src/pages/contact/Contact.js
+++ src/pages/contact/Contact.js
@@ -5,15 +5,16 @@ export default function Contact () {
         <div id='contact-container'>
             <link rel="stylesheet" href="https://cdn.jsdelivr.net/gh/devicons/devicon@v2.15.1/devicon.min.css"></link>
             <h3>Contact Me</h3>
-            <p>Send an email or Facebook message to set up your sessions.
+            <img id='client-image' alt='client contact' src='1679288947320.jpg'/>
+            <p>
+                Send an email or Facebook message to set up your sessions.
                 Be sure to check me out on Instagram!
             </p>
-            <img id='user-contact' alt='user-contact' src='1679288947320.jpg'/>
-            <div id='social-media-container'>
-                <a href='https://www.facebook.com/JulieBrownLIB'><img  alt='facebook' src='facebook.png'/></a>
-                <img alt='mail' src='mail.png'/>
-                <a href='https://www.instagram.com/julie200brown/'><img alt='instagram' src='instagram.png'/></a>
-            </div>
+                <div id='social-media-container'>
+                    <a href='https://www.facebook.com/JulieBrownLIB'><img  alt='facebook' src='facebook.png'/></a>
+                    <img alt='mail' src='mail.png'/>
+                    <a href='https://www.instagram.com/julie200brown/'><img alt='instagram' src='instagram.png'/></a>
+                </div>
             <div id='about-nav'/>
         </div>
     )
