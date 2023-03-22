078564c264c910fd476a9ced3dd1860cbbe680a3
AlexBrown4020 <balex3497@gmail.com>
2023-03-22 16:44:11 +0900

    Feat: Adjust layout colours and font shadow

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -3,8 +3,8 @@ import './footer.css';
 export default function Footer () {
     return (
         <footer>
-            <h6>Life in Balance</h6>
-            <h6>Copyright © 2023, All Rights Reserved.</h6>
+            <p>Life in Balance</p>
+            <p>Copyright © 2023, All Rights Reserved.</p>
         </footer>
     )
 }
