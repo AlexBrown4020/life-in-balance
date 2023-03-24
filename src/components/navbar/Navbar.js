aae0fc90375cf9a956d2f666487e65ad300a91c0
AlexBrown4020 <balex3497@gmail.com>
2023-03-24 14:06:28 +0900

    Feat: Reposition navbar container

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -3,16 +3,13 @@ import { useState } from 'react';
 import './navbar.css';
 
 export default function Navbar () {
-    const [burgerClass, setBurgerClass] = useState('burger-bar unclicked');
     const [isMenuClicked, setIsMenuClicked] = useState(false);
     const [menu, setMenu] = useState('nav-container hidden');
 
     const updateMenu = () => {
         if(!isMenuClicked) {
-            setBurgerClass('burger-bar clicked');
             setMenu('nav-container visible');
         } else {
-            setBurgerClass('burger-bar unclicked');
             setMenu('nav-container hidden');
         }
         setIsMenuClicked(!isMenuClicked);
@@ -30,9 +27,9 @@ export default function Navbar () {
                 <button><a href='#about-nav'>About</a></button>
             </div>
             <div className='burger-menu' onClick={updateMenu}>
-                <div className={burgerClass}></div>
-                <div className={burgerClass}></div>
-                <div className={burgerClass}></div>
+                <div className='burger-bar'></div>
+                <div className='burger-bar'></div>
+                <div className='burger-bar'></div>
             </div>
         </nav>
     )
