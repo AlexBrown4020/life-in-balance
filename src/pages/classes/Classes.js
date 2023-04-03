8138225f89ee4e8b261be237d8f3b1d5826c1f06
AlexBrown4020 <balex3497@gmail.com>
2023-04-03 15:30:27 +0900

    Feat: Adjust class restrictions according to client preferences

--- src/pages/classes/Classes.js
+++ src/pages/classes/Classes.js
@@ -12,7 +12,9 @@ export default function Classes () {
                 <ul>
                     <li>Monthly membership at £6 per class</li>
                     <li>Or £7 per class</li>
+                    <li>Access to recordings throughout the month</li>
                 </ul>
+                <p className='classes-info'>*Excluding bank holidays</p>
             </section>
             <section>
                 Zoom Session:
@@ -26,12 +28,10 @@ export default function Classes () {
                     <li>£35-£45 per 75 minutes</li>
                     <li>Equipment available on request</li>
                 </ul>
+                <p className='classes-info'>*Limited by distance</p>
             </section>
             <p className='classes-info'>
                 *Block bookings and group sessions are available
-            </p>
-            <p className='classes-info'>
-                **Home visits are limited by distance/location
             </p>
             <div id='contact-nav'/>
         </div>
