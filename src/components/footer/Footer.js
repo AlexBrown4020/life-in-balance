7ab42d78e536cf5b7ed7b92eacb295696f26986b
AlexBrown4020 <balex3497@gmail.com>
2024-02-01 20:00:33 +0900

    Feat: Update footer date

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -3,7 +3,7 @@ import './footer.css';
 export default function Footer () {
     return (
         <footer>
-                <h6>Copyright © 2023,<br></br>All Rights Reserved.</h6>
+                <h6>Copyright © 2024,<br></br>All Rights Reserved.</h6>
                 <h6>Life in Balance</h6>
                 <h6>Designer <br></br>portfolio: <a href='https://alexbrown-portfolio.netlify.app/'>Alex Brown</a></h6>
         </footer>
